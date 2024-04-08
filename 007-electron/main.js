5341ca5f43ec51f215fc180eaa8d2646a593af18
VunboYao <yaoyuanbo@lansijie.com>
2024-04-08 20:49:00 +0800

    feat: electron 测试

--- 007-electron/main.js
+++ 007-electron/main.js
@@ -2,7 +2,7 @@
 app: 控制应用程序的事件生命周期
 BrowserWindow: 创建并控制浏览器窗口
 */
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
 const path = require('path')
 
 const createWindow = () => {
@@ -23,18 +23,18 @@ const createWindow = () => {
   win.webContents.openDevTools()
 }
 
+// 当 Electron 完成初始化并准备创建浏览器窗口时调用此方法
 app.whenReady().then(() => {
+  ipcMain.handle('ping', () => 'pong')
   createWindow()
 
-  // macOS中点击dock图标时没有已打开的窗口时，创建一个窗口
+  // macOS中没有开启的窗口时，创建一个窗口
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
 
-// 除了 macOS 外，当所有窗口都被关闭的时候退出程序。 因此, 通常
-// 对应用程序和它们的菜单栏来说应该时刻保持激活状态, 
-// 直到用户使用 Cmd + Q 明确退出
+// (Windows & Linux)，当所有窗口都被关闭的时候退出程序。
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
 })
